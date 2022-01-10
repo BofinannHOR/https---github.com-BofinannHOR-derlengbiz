04de66747ca9412a84d8b444fac9775f873cf055
BofinannHOR <bofinannhor@gmail.com>
2022-01-10 15:55:40 +0700

    change font under h1

--- components/OurService.js
+++ components/OurService.js
@@ -7,7 +7,7 @@ const OurService = () => {
     <div id="ourservice" className="section-two text-slate-700  p-3 m-4">
       <div className="items-center flex flex-col">
         <h1 className="mb-3 pt-28">ផ្តល់ជម្រើសដ៏សំបូរបែបសម្រាប់អ្នក</h1>{" "}
-        <p className="text-center text-base md:text-base lg:text-lg">
+        <p className="text-center text-base md:text-base lg:text-lg leading-loose w-4/6">
           ទទួលយកនូវបទពិសោធន៍ថ្មីពី ដើរលេង
           ដែលផ្តល់ជូននូវជម្រើសដ៏សំបូរបែបដល់អ្នកទេសចរកម្ពុជា
         </p>
